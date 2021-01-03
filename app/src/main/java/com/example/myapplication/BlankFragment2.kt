979d53216441d832b4b17bd8a3a77d732ab9fb8c
Knut Tyse <knuttyse@gmail.com>
2021-01-03 20:47:09 +0100

    fix move

--- app/src/main/java/com/example/myapplication/BlankFragment2.kt
+++ app/src/main/java/com/example/myapplication/BlankFragment2.kt
@@ -1,82 +1,86 @@
 package com.example.myapplication
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatImageView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyColumnForIndexed
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme.colors
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.MeasureBlock
 import androidx.compose.ui.layout.WithConstraints
-import androidx.compose.ui.platform.AmbientDensity
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.DensityAmbient
-import androidx.compose.ui.res.loadImageResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.viewinterop.viewModel
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
-import kotlin.math.min
+import com.github.bhlangonijr.chesslib.*
+import com.github.bhlangonijr.chesslib.Board
 
-class Square(context: Context) : AppCompatImageView(context) {
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(
-            widthMeasureSpec,
-            widthMeasureSpec
-        ) // This is the key that will make the height equivalent to its width
-    }
-}
-
-val light: Color = Color.White
-val dark: Color = Color.Black
-
-val upperRow: List<Color> = listOf(light, dark, light, dark, light, dark, light, dark)
-val lowerRow = listOf(dark, light, dark, light, dark, light, dark, light)
-
-val squares: List<Color> = listOf(
-    upperRow, lowerRow,
-    upperRow, lowerRow,
-    upperRow, lowerRow,
-    upperRow, lowerRow
-).flatten()
-
-val flippedSquares = listOf(
-    lowerRow, upperRow,
-    lowerRow, upperRow,
-    lowerRow, upperRow,
-    lowerRow, upperRow
-).flatten()
-
-
-data class Blank2State(
-    val flipped: Boolean,
-    val pieces: List<Piece?>
+data class SquareData(
+    val square: Square,
+    val piece: Piece,
+    val isSelected: Boolean,
+    val isCandidate: Boolean
 )
 
+val r8 =
+    listOf(Square.A8, Square.B8, Square.C8, Square.D8, Square.E8, Square.F8, Square.G8, Square.H8)
+val r7 =
+    listOf(Square.A7, Square.B7, Square.C7, Square.D7, Square.E7, Square.F7, Square.G7, Square.H7)
+val r6 =
+    listOf(Square.A6, Square.B6, Square.C6, Square.D6, Square.E6, Square.F6, Square.G6, Square.H6)
+val r5 =
+    listOf(Square.A5, Square.B5, Square.C5, Square.D5, Square.E5, Square.F5, Square.G5, Square.H5)
+val r4 =
+    listOf(Square.A4, Square.B4, Square.C4, Square.D4, Square.E4, Square.F4, Square.G4, Square.H4)
+val r3 =
+    listOf(Square.A3, Square.B3, Square.C3, Square.D3, Square.E3, Square.F3, Square.G3, Square.H3)
+val r2 =
+    listOf(Square.A2, Square.B2, Square.C2, Square.D2, Square.E2, Square.F2, Square.G2, Square.H2)
+val r1 =
+    listOf(Square.A1, Square.B1, Square.C1, Square.D1, Square.E1, Square.F1, Square.G1, Square.H1)
+
+
+val normalRows = listOf(
+    r8, r7, r6, r5, r4, r3, r2, r1
+)
+val flippedRows = listOf(
+    r1.reversed(), r2.reversed(), r3.reversed(), r4.reversed(),
+    r5.reversed(), r6.reversed(), r7.reversed(), r8.reversed()
+)
+
+data class Blank2State(
+    val board: Board,
+    val selectedSquare: Square = Square.NONE,
+    val promoteSquare: Square? = null
+) {
+    val flipped = board.sideToMove == Side.BLACK
+    val legalMoves = board.legalMoves()
+    val possibleMoves = legalMoves.filter { it.from === selectedSquare }
+    val candidateSquares = possibleMoves.map { it.to }
+
+    val squares = (if (flipped) flippedRows else normalRows).map { rank ->
+        rank.map { square ->
+            SquareData(
+                square,
+                board.getPiece(square),
+                square == selectedSquare,
+                candidateSquares.contains(square)
+            )
+        }
+    }
+}
 
 class Blank2ViewModel(initialState: Blank2State) : ViewModel() {
     val state = MutableLiveData<Blank2State>(initialState)
@@ -84,16 +88,41 @@ class Blank2ViewModel(initialState: Blank2State) : ViewModel() {
     private fun setState(f: (Blank2State) -> Blank2State) {
         val value = state.value
 
-        if (value == null) {
-            state.postValue(value)
-        } else {
+        if (value != null) {
             state.postValue(f(value))
         }
     }
 
-    fun flip() {
-        setState { it.copy(flipped = !it.flipped) }
+    fun clickSquare(data:SquareData) {
+        val value = state.value
+        val currentSelected = value?.selectedSquare
+        val square = data.square
+
+        if (currentSelected == data.square) {
+            // Unselect piece
+            setState { it.copy(selectedSquare = Square.NONE) }
+        } else if (value?.candidateSquares?.contains(square) == true) {
+            // Is move
+            val possible = value.possibleMoves.filter { it.to == square }
+
+            if (possible.size > 1) {
+                setState { it.copy(promoteSquare = data.square) }
+            } else {
+                val board = value.board.clone()
+                board.doMove(possible.first())
+                setState {
+                    it.copy(board = board, selectedSquare = Square.NONE, null)
+                }
+            }
+
+        } else {
+            setState { it.copy(selectedSquare = square) }
+        }
     }
+
+    /*fun flip() {
+        setState { it.copy(flipped = !it.flipped) }
+    }*/
 }
 
 class Blank2ViewModelFactory(val initialState: Blank2State) : ViewModelProvider.Factory {
@@ -106,30 +135,29 @@ class Blank2ViewModelFactory(val initialState: Blank2State) : ViewModelProvider.
     }
 }
 
-enum class Piece(val drawable: Int) {
-    BK(R.drawable.chess_kdt45),
-    BQ(R.drawable.chess_qdt45),
-    BR(R.drawable.chess_rdt45),
-    BB(R.drawable.chess_bdt45),
-    BN(R.drawable.chess_ndt45),
-    BP(R.drawable.chess_pdt45),
-
-    WK(R.drawable.chess_klt45),
-    WQ(R.drawable.chess_qlt45),
-    WR(R.drawable.chess_rlt45),
-    WB(R.drawable.chess_blt45),
-    WN(R.drawable.chess_nlt45),
-    WP(R.drawable.chess_plt45)
-}
-
-val pieces = Piece.values()
-
-val testPieces: List<Piece?> = (0..63).map(pieces::getOrNull)
+val Piece.drawable: Int
+    get() = when (this) {
+        Piece.WHITE_PAWN -> R.drawable.chess_plt45
+        Piece.WHITE_KNIGHT -> R.drawable.chess_nlt45
+        Piece.WHITE_BISHOP -> R.drawable.chess_blt45
+        Piece.WHITE_ROOK -> R.drawable.chess_rlt45
+        Piece.WHITE_QUEEN -> R.drawable.chess_qlt45
+        Piece.WHITE_KING -> R.drawable.chess_klt45
+        Piece.BLACK_PAWN -> R.drawable.chess_pdt45
+        Piece.BLACK_KNIGHT -> R.drawable.chess_ndt45
+        Piece.BLACK_BISHOP -> R.drawable.chess_bdt45
+        Piece.BLACK_ROOK -> R.drawable.chess_rdt45
+        Piece.BLACK_QUEEN -> R.drawable.chess_qdt45
+        Piece.BLACK_KING -> R.drawable.chess_kdt45
+        Piece.NONE -> R.drawable.chess_empty
+    }
 
 class BlankFragment2 : Fragment() {
 
     private val model: Blank2ViewModel by viewModels<Blank2ViewModel> {
-        val state = Blank2State(false, testPieces)
+        val board = Board()
+        board.doNullMove()
+        val state = Blank2State(board, Square.NONE, null)
         Blank2ViewModelFactory(state)
     }
 
@@ -140,7 +168,7 @@ class BlankFragment2 : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                Board()
+                Board(model)
             }
         }
     }
@@ -153,37 +181,56 @@ class BlankFragment2 : Fragment() {
         Navigation.findNavController(view).navigate(R.id.action_blankFragment2_to_blankFragment)
     }
 }
-
+/*
 @Preview
 @Composable
 fun PreviewBoard() {
     Board()
-}
+}*/
 
 
 @Composable
-fun Board() {
+fun Board(viewModel: Blank2ViewModel = viewModel()) {
+    val state: Blank2State? by viewModel.state.observeAsState()
+    val clickSquare: (SquareData)->Unit = {viewModel.clickSquare(it)}
+    state?.let {
+        Board2(it, clickSquare)
+    } ?: Board2(state = Blank2State(Board(), Square.NONE, null), clickSquare)
+}
+
+@Composable
+private fun Board2(state: Blank2State, clickSquare: (SquareData) -> Unit) {
+    val rankRange = 0..7
+    val columnRange = 0..7
+
     Column(Modifier.fillMaxWidth().fillMaxHeight()) {
         WithConstraints {
-            /*val boardSize =
-                (min(constraints.maxHeight, constraints.maxWidth)
-                    .toFloat() / 3).dp
-            val squareSize = boardSize / 8*/
-            Column {
-                (1..8).forEach { r -> // rows
-                    Row {
-                        (1..8).forEach { c -> // columns
-                            val color =
-                                if (c.rem(2) == r.rem(2))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                rankRange.forEach { r -> // rows
+                    Row(
+                        //modifier = Modifier.weight(1f)
+                    ) {
+                        columnRange.forEach { c -> // columns
+                            val color =//Color.LightGray
+                                if (c.rem(2) != r.rem(2))
                                     Color.LightGray
                                 else Color.White
+
+                            val squareData = state.squares[r][c]
+                            val pieceDrawable = squareData.piece.drawable
+
                             Box(
                                 modifier =
-                                    Modifier.weight(1f)
-                                            .background(color)
+                                Modifier.weight(1f)
+                                    .background(color)
+                                    .clickable(onClick = {
+                                        clickSquare(squareData)
+                                    })
                             ) {
                                 Image(
-                                    vectorResource(id = R.drawable.chess_bdt45),
+                                    vectorResource(id = pieceDrawable),
                                     modifier = Modifier.align(Alignment.Center)
                                 )
                             }
